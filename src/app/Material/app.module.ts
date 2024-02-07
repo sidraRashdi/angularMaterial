51c790a0a822ae854d1248a5b0d6e6d88709eaa8
sidraRashdi <rashdimuntaha557@gmail.com>
2024-02-07 11:35:33 +0500

    practice on menu sidenav progressBar icons

--- src/app/Material/app.module.ts
+++ src/app/Material/app.module.ts
@@ -3,8 +3,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
-// import { AppComponent } from './app.component';
-const materialComponents = [MatSlideToggleModule, MatButtonModule];
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+const materialComponents = [
+  MatSlideToggleModule,
+  MatButtonModule,
+  MatIconModule,
+  MatButtonToggleModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule,
+];
 @NgModule({
   // declarations: [AppComponent],
   imports: [CommonModule, materialComponents],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,20 @@ import { MaterialModule } from './Material/app.module';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, MaterialModule],
-  templateUrl: 'app.component.html',
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angularMaterial';
+  // Declare the alertsEnabled property
+  notify = 0;
+  alertsEnabled: boolean = true; // You can set the initial value based on your logic
+  showSpinner = false;
+  loadData() {
+    this.showSpinner = true;
+    setTimeout(() => {
+      this.showSpinner = false;
+    }, 5000);
+  }
+  opened = false;
 }
